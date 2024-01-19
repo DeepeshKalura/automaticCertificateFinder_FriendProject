4b3d9fce7360e57760b5b3a0a1f18dc578cb649c
DeepeshKalura <deepeshkalurs@gmail.com>
2024-01-19 20:07:20 +0530

    create the folder in the drive.py

--- drive.py
+++ drive.py
@@ -3,55 +3,29 @@ import io
 
 from dotenv import load_dotenv, find_dotenv
 from google_mine import create_service
-from googleapiclient.http import MediaIoBaseDownload
-
 
 load_dotenv(find_dotenv())
-SCOPE = ['https://www.googleapis.com/auth/drive.readonly']
+SCOPE = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.readonly"]
 API_NAME = 'drive'
 API_VERSION = 'v3'
 
-drive_service = create_service(os.getenv('CLIENT_SECRET_FILE'), API_NAME, API_VERSION, SCOPE)
+drive_service = create_service('credentials.json', API_NAME, API_VERSION, SCOPE)
 
-print(dir(drive_service))
+# simple create the folder in drive
 
-folder_link = "https://drive.google.com/drive/folders/1my5S5mOPaIk7jkQOv-P62_dpLwAmFpnm"
-folder_id = folder_link.split('/')[-1]
-# print(folder_id)
+folderIWillCreate =  ["TeraName", "MeraName", "SabkaName"]
 
-page_token = None
-results = drive_service.files().list(
-    spaces='drive',
-    fields="files(id, name)",
-    pageToken=page_token
-).execute()
+for folder in folderIWillCreate:
+    file_metadata = {
+        "name": folder,
+        "mimeType": "application/vnd.google-apps.folder",
+        "parents": "My Drive"
+    }
 
-print("I got excuted")
-files = results.get('files', [])
+    drive_service.files().create(body=file_metadata).execute()
 
+print("Done, Now Check the google drive")
 
-for file in files:
-    print("Lol never come inside")
-    print(file['name'], file['id'])
-
-
-
-
-# try :
-#     request = service.files().get_media(fileId = folderId)
-#     fh = io.BytesIO()
-#     downloader = MediaIoBaseDownload(fh, request)
-
-#     done = False 
-
-#     while done is False:
-#         status , done = downloader.next_chunk()
-
-#     with open('2.pdf', 'wb') as f:
-#         f.write(fh.getvalue())
-
-# except Exception as e:
-#     print(f"{e}")
 
 
 
