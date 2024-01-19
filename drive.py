7360e115bdba61b59f671d421189327314760035
DeepeshKalura <deepeshkalurs@gmail.com>
2024-01-20 02:01:03 +0530

    Update Google Drive API scope and file download functionality

--- drive.py
+++ drive.py
@@ -3,67 +3,61 @@ import io
 
 from dotenv import load_dotenv, find_dotenv
 from google_mine import create_service
-from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseDownload
 
 load_dotenv(find_dotenv())
-SCOPE = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.readonly"]
+SCOPE = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.appdata",  "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive.metadata", "https://www.googleapis.com/auth/drive.metadata.readonly",  "https://www.googleapis.com/auth/drive.readonly"]
 API_NAME = 'drive'
 API_VERSION = 'v3'
 
-drive_service = create_service('credentials.json', API_NAME, API_VERSION, SCOPE)
+drive_service = create_service(os.getenv("CLIENT_SECRET_FILE"), API_NAME, API_VERSION, SCOPE)
 
-# simple upload file in the folder
-
-folder_id = "1B5iwnts7AYoYeQc9-5oQaZp2qLfMW22b"
-file_metadata = {
-    'name': '1-100-1.pdf',
-    'mineType': 'application/pdf',
-    'parents': [folder_id] 
-}
-
-try:
-
-    media = MediaFileUpload('1-100-1.pdf', mimetype='application/pdf')
-except Exception as e:
-    
-    print(f"media is not right:  {e}")
-
-try:
-    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print(file)
-except Exception as e:
-    print(f"file is not right: {e}")
-
-# download this file folder 
+folder_id = "1my5S5mOPaIk7jkQOv-P62_dpLwAmFpnm"
 
 # First with the file link 
 
-file_id = '10n2-mQLoLftelod-Mu1EefUFOLjfRHK5'
+def get_pdf_from_link (file_id):
 
-request = drive_service.files().get_media(fileId = file_id)
+    # file_id = '10n2-mQLoLftelod-Mu1EefUFOLjfRHK5'
 
-fh = io.BytesIO()
-downloader = MediaIoBaseDownload(fd=fh, request=request)
+    request = drive_service.files().get_media(fileId = file_id)
 
-done = False
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fd=fh, request=request)
 
-while not done:
-    status , done = downloader.next_chunk()
-    print('Download Progress {0} %'.format(status.progress() * 100))
+    done = False
+
+    while not done:
+        status , done = downloader.next_chunk()
+        print('Download Progress {0} %'.format(status.progress() * 100))
 
 
-fh.seek(0)
+    fh.seek(0)
 
-with open('downloadPdf.pdf', 'wb') as f:
-    f.write(fh.read())
-    f.close()
+    with open('downloadPdf.pdf', 'wb') as f:
+        f.write(fh.read())
+        f.close()
 
 
 # I have to find the file_id  from the folder_id
     
-print(f'folder id is :  {folder_id}')
+def folder_to_files_code(folder_id):
+    query = f"'{folder_id}' in parents and mimeType = 'application/pdf'"
+    count = 0
+    nextPageToken = "FirstTime"
+    while nextPageToken != None:
+        try:
+            if(nextPageToken == "FirstTime"):
+                response =  drive_service.files().list(q=query, orderBy="name").execute()
+            else:
+                response =  drive_service.files().list(q=query, pageToken=nextPageToken, orderBy="name").execute()
+            
+            nextPageToken = response.get('nextPageToken')
+            files = response.get('files')
 
-response =  drive_service.files().list(q=f"'{folder_id}' in parents and mimeType = 'application/pdf'").execute()
-
-for file in response.get('files', []):
-    print(f"Found file: {file.get('name')} with id: {file.get('id')}")
+            for file in files:
+                # print(f"file name is : {file.get('name')} and id is : {file.get('id')}")
+                count += 1
+            
+        except Exception as e:
+            print(e)
