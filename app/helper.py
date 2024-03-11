9c3766d253dfc819a9e9140ccbf4b313f509ca1f
DeepeshKalura <deepeshkalurs@gmail.com>
2024-03-11 09:31:05 +0530

    Add remove_query_param function to helper.py

---
+++ app/helper.py
@@ -0,0 +1,20 @@
+def remove_query_param(url: str, param: str) -> str:
+    """
+    Removes the specified query parameter from the given URL.
+
+    Args:
+        url (str): The URL from which the query parameter needs to be removed.
+        param (str): The query parameter to be removed.
+
+    Returns:
+        str: The updated URL with the query parameter removed.
+    """
+    param_start = url.find(param)
+    
+    if param_start != -1:
+        param_end = url.find('&', param_start)
+        if param_end == -1:
+            param_end = len(url)
+        url = url[:param_start] + url[param_end:]
+    
+    return url
