6999f9abea112c8817c84c1c3c053f1def0a7f2e
DeepeshKalura <deepeshkalurs@gmail.com>
2024-03-12 22:01:40 +0530

    Add flow logic for handling different scenarios

---
+++ app/flow/__init__.py
@@ -0,0 +1,24 @@
+# Best case sernerio
+# convert folder_link into the id 
+# --> then check is folder_id exit in a database or not 
+    # --> if yes then check the user exit in the database or not 
+        # --> if yes then we will going to get the url of the pdf file and we will download it and show the user
+
+# Optimization case sernerio
+# -->  in case that folder_id exit in a database but not the user then what we will going to do.
+
+# Worst case sernerio
+# convert folder_link into the id
+#   --> then check folder_id exit in a database or not
+#   --> if no then we will go the folder check every link that that present in the folder 
+#   --> can user ocr or multimodel to check it and stored every value in the database so we can get the best case senario
+#   --> if no we does not got the user name of the show user that we does not find your certificate.
+
+# Optimized Worst case sernerio
+# It is the advance feature where we uses binary search rather than linear search to find the user name in the pdf file.
+
+
+# Extra things which we have to do is :-
+# Protection from dns with machine learning
+# Cap protection for the drive api
+
