df00cada4085a10973876b09e2dfb151ea6960b8
DeepeshKalura <deepeshkalurs@gmail.com>
2024-03-12 22:01:53 +0530

    Refactor cache and logs modules

--- app/logs.py
+++ app/logs.py
@@ -1,17 +1,17 @@
-# import logging
+import logging
 
-# # level: DEBUG, INFO, WARNING, ERROR, CRITICAL
+# level: DEBUG, INFO, WARNING, ERROR, CRITICAL
 
-# logging.basicConfig(level=logging.INFO, filename='appFlow.log', filemode='w', format='%(asctime)s  - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(level=logging.INFO, filename='appFlow.log', filemode='w', format='%(asctime)s  - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
-# def infoLog(message: str):
-#     logging.info(message)
+def infoLog(message: str):
+    logging.info(message)
 
-# def warningLog(message: str):
-#     logging.warning(message)
+def warningLog(message: str):
+    logging.warning(message)
 
-# def errorLog(message: str):
-#     logging.error(message, exc_info=True)
+def errorLog(message: str):
+    logging.error(message, exc_info=True)
 
-# def criticalLog(message: str):
-#     logging.critical(message)
+def criticalLog(message: str):
+    logging.critical(message)
